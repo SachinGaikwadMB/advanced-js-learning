234d5ef145f1eda73d5556832509e0d5475abe96
= <=>
2023-01-31 17:27:14 +0530

    initial commit

---
+++ js/var-let-const.js
@@ -0,0 +1,8 @@
+var message = "Hello";
+var message = "Hello Welcome";
+//var are global in scope
+
+console.log(message);
+
+let name = 'sachin';
+// let name = 'gaikwad'; canot declare  and local in scope
